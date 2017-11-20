5857a61afebaac0cf5690f81d554003734c056dc
vidahaha <937634115@qq.com>
2017-11-20 19:07:59 +0800

    add ts-demo

---
+++ ts-first-demo.ts
@@ -0,0 +1,9 @@
+function greeter( person: string ) {
+    return 'hello ' + person
+}
+
+let user = 'vida haha'
+let arr = [1, 2, 3]
+
+console.log( greeter( user ) ) // ​​​​​hello vida haha​​​​​
+console.log( greeter( [1, 2, 3] ) ) // 报错
